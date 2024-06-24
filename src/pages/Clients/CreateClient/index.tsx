5d7897eb17daae7a2d81328729cd0c1d9cfd0d48
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2024-06-23 21:53:57 -0300

    ta

--- src/pages/Clients/CreateClient/index.tsx
+++ src/pages/Clients/CreateClient/index.tsx
@@ -1,7 +1,86 @@
-import { Text } from "react-native";
+import { Text, TextInput, TouchableOpacity, View, Alert } from "react-native";
+import HeaderOnlyBack from "../../../components/Headers/HeaderOnlyBack";
+import { useState } from "react";
+import { StyledComponent } from "nativewind";
 
 export default function CreateClient() {
-    return (
-        <Text>CreateClient</Text>
-    );
+  const [client, setClient] = useState("");
+  const [company, setCompany] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  function sendData() {
+    console.log(client, email, company, phone);
+    Alert.alert("Sucesso", "Cliente cadastrado com sucesso!");
+  }
+
+  return (
+    <View className="mt-10 items-center">
+      <HeaderOnlyBack title="Criar Cliente" />
+      <View className="mt-4 w-96">
+        <Text className="text-left font-bold text-lg text-black mb-2">
+          Nome
+        </Text>
+        <View className="items-center rounded-md justify-center h-12 border-2 border-gray-200 w-full">
+          <TextInput
+            onChangeText={setClient}
+            value={client}
+            placeholder="Digite o nome do cliente"
+            autoCapitalize="none"
+            className="flex-1 onFocus:border-inputColor border-black h-12 rounded-md items-center justify-between p-2"
+          />
+        </View>
+        <Text className="text-left font-bold text-lg mt-5 text-black mb-2">
+          E-mail
+        </Text>
+        <View className="items-center rounded-md justify-center h-12 border-2 border-gray-200 w-full">
+          <TextInput
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Digite o e-mail do cliente"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            className="flex-1 onFocus:border-inputColor border-black h-12 rounded-md items-center justify-between p-2"
+          />
+        </View>
+        <Text className="text-left font-bold text-lg mt-5 text-black mb-2">
+          Empresa
+        </Text>
+        <View className="items-center rounded-md justify-center h-12 border-2 border-gray-200 w-full">
+          <TextInput
+            onChangeText={setCompany}
+            value={company}
+            placeholder="Digite a empresa do cliente"
+            autoCapitalize="none"
+            className="flex-1 onFocus:border-inputColor border-black h-12 rounded-md items-center justify-between p-2"
+          />
+        </View>
+        <Text className="text-left font-bold text-lg mt-5 text-black mb-2">
+          Telefone
+        </Text>
+        <View className="items-center rounded-md justify-center h-12 border-2 border-gray-200 w-full">
+          <TextInput
+            onChangeText={setPhone}
+            value={phone}
+            placeholder="Digite o telefone do cliente"
+            keyboardType="phone-pad"
+            autoCapitalize="none"
+            className="flex-1 onFocus:border-inputColor border-black h-12 rounded-md items-center justify-between p-2"
+          />
+        </View>
+        <View className="flex-1 mt-10 w-96 justify-end">
+          <StyledComponent component={TouchableOpacity}>
+            <TouchableOpacity
+              className="h-14 bg-primary w-auto rounded-md items-center mb-10 justify-center"
+              onPress={() => sendData()}
+            >
+              <Text className="text-white font-medium font-Principal text-md">
+                Criar Orçamento
+              </Text>
+            </TouchableOpacity>
+          </StyledComponent>
+        </View>
+      </View>
+    </View>
+  );
 }
--- src/pages/LoginScreen/index.tsx
+++ src/pages/LoginScreen/index.tsx
@@ -1,17 +1,58 @@
-import { View, Image, Text, Button } from "react-native";
+import { View, Image, Text, Button, TextInput, TouchableOpacity } from "react-native";
 import EmailInput from "../../components/LoginInputs";
 import { PasswordInput } from "../../components/LoginInputs";
 import { LoginButton } from "../../components/LoginInputs";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faEnvelope, faEye, faEyeSlash, faLock } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Figure = require("../../assets/images/login.png");
 
 export default function LoginScreen() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [secureText, setSecureText] = useState(true);
+
+    function toggleShowPassword() {
+        setSecureText(!secureText);
+    }
+
+
     return (
         <View className="flex-1 items-center justify-center mb-60">
             <Image source={Figure} />
             <Text className="font-bold text-2xl mt-10">Acesse sua conta</Text>
-            <EmailInput />
-            <PasswordInput />
+            <View className="bg-inputColor border-2 onFocus:border-inputColor  border-gray-200 mt-10 w-80 h-12 rounded-md flex-row items-center px-3">
+            <View className="bg-white w-7 h-7 rounded-full items-center justify-center">
+                <FontAwesomeIcon icon={faEnvelope} style={{color: "#6359e7"}} />
+            </View>
+            <TextInput
+                onChangeText={setEmail}
+                value={email}
+                placeholder="Digite seu e-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                className="flex-1 ml-2 text-center text-black font-bold"
+            />
+        </View>
+        <View className="bg-inputColor border-2 onFocus:border-inputColor border-gray-200 mt-5 w-80 h-12 rounded-md flex-row items-center px-3">
+            <View className="bg-white w-7 h-7 rounded-full items-center justify-center">
+                <FontAwesomeIcon icon={faLock} style={{color: "#6359e7"}} />
+            </View>
+            <TextInput
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Digite sua senha"
+                keyboardType="default"
+                autoCapitalize="none"
+                className="flex-1 ml-2 text-center text-black font-bold"
+                secureTextEntry={secureText}
+            />
+            <TouchableOpacity onPress={toggleShowPassword}>
+                <FontAwesomeIcon icon={secureText ? faEye : faEyeSlash} style={{color: "#6359e7"}} />
+            </TouchableOpacity>
+        </View>
             <LoginButton/>
         </View>
      
